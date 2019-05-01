55dfdbc34d95b84082677d46e29a94e6737e6c1f
Ryota Suwa <r.suwa@isl.cs.waseda.ac.jp>
2019-05-01 15:25:29 +0900

    Add method isAttackable

--- src/agent/ActionA.java
+++ src/agent/ActionA.java
@@ -2,6 +2,7 @@ package agent;
 
 import environment.Field1;
 import core.*;
+import static java.lang.Math.abs;
 
 //他に使いたいライブラリがあればここに書く. 例 java.util.*;
 
@@ -9,7 +10,7 @@ import core.*;
 public class ActionA {
 	Field1 field;
 	Sfmt rnd;
-	
+
 	//フィールドはここに書く
 	
 
@@ -27,6 +28,12 @@ public class ActionA {
 		
 		//返り値別の行動一覧: 0(上移動), 1(右移動), 2(下移動), 3(左移動), 4(上攻撃), 5(右攻撃), 6(下攻撃), 7(左攻撃), それ以外(何もしない)
 		return (int)(rnd.NextUnif()*9);
+	}
+
+	private boolean isAttackable(int pos[], int e_pos[]) {
+		// 相手の位置が次の移動時に攻撃範囲にくる場合にtrueを返すメソッド
+		if (abs(pos[0] - e_pos[0]) >= 2 && abs(pos[1] - e_pos[1]) >= 2) return false;
+		return true;
 	}
 
 }
